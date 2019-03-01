22a883d198ad5f7c5e53693cdc1a211eb61513b8
triprog-3 <triprog-3@lab.local>
2019-03-01 13:11:29 +0100

    verificatriangolo

---
+++ primaprova/ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lato1 = AskandCheckNumber("inserisci primo lato");
+            int lato2 = AskandCheckNumber("inserisci primo lato");
+            int lato3 = AskandCheckNumber("inserisci primo lato");
+
+          
+
+            bool sumOK = lato1 + lato2 > lato3 && lato1 + lato3 > lato2 && lato2 + lato3 > lato1;
+                bool difOk = lato1 < Math.Abs(lato2 - lato3) && lato2 < Math.Abs(lato1 - lato3) && lato3 < Math.Abs(lato2 - lato1);
+            if (sumOK && difOk)
+            {
+                Console.WriteLine("è un triangolo");
+            }
+            else
+            {
+                Console.WriteLine("non è un triangolo");
+            }
+
+            Console.ReadLine();
+        }
+        /// <summary>
+        /// Nostra all'utente il messaggio e prova
+        /// convertire il valore inserito in input in un intero
+        /// </summary>
+        /// <param name="message">Messaggio mostrato all'utente</param>
+        /// <returns></returns>
+        static int AskandCheckNumber (string message)
+        {
+        
+            Console.WriteLine("message");
+            string input = Console.ReadLine();
+            bool conversionOk = int.TryParse(input, out int convertedValue);
+
+            if (!conversionOk)
+                Console.WriteLine("l'imput non valido");
+
+            if (convertedValue <= 0)
+                Console.WriteLine("il valore deve essere positivo");
+
+            return convertedValue;
+        }
+    }
+}
